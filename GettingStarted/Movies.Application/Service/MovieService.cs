eafb627963d31d443968ca6e8ebb2224050332c0
joshuauche <contactuche02@gmail.com>
2025-01-17 21:17:01 +0100

    moved business logic to the MovieSevice class

---
+++ GettingStarted/Movies.Application/Service/MovieService.cs
@@ -0,0 +1,53 @@
+﻿using Movies.Application.Models;
+using Movies.Application.Repository;
+
+namespace Movies.Application.Service
+{
+    public class MovieService : IMovieService
+    {
+
+        private readonly IMovieRepository _movieRepository;
+
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public Task<bool> CreateAsync(Movie movie)
+        {
+            return _movieRepository.CreateAsync(movie);
+        }
+
+        public Task<bool> DeleteByIdAsync(Guid id)
+        {
+            return _movieRepository.DeleteByIdAsync(id);
+        }
+
+        public Task<IEnumerable<Movie>> GetAllAsync()
+        {
+            return _movieRepository.GetAllAsync();
+        }
+
+        public Task<Movie?> GetByIdAsync(Guid id)
+        {
+            return _movieRepository.GetByIdAsync(id);
+        }
+
+        public Task<Movie?> GetBySlugAsync(string slug)
+        {
+            return _movieRepository.GetBySlugAsync(slug);
+        }
+
+        public async Task<Movie?> UpdateAsync(Movie movie)
+        {
+            var movieExist = await _movieRepository.ExistsByIdAsync(movie.Id);
+
+            if (movieExist)
+                return null;
+
+            await _movieRepository.UpdateAsync(movie);
+
+            return movie;
+        }
+    }
+}

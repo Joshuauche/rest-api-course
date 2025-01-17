168e9ea5e8dfa99eaa90d4329873eff1dad7a850
joshuauche <contactuche02@gmail.com>
2025-01-17 09:57:15 +0100

    added implementation for other methods

--- GettingStarted/Movies.Application/Repository/MovieRepository.cs
+++ GettingStarted/Movies.Application/Repository/MovieRepository.cs
@@ -7,16 +7,16 @@ namespace Movies.Application.Repository
     public class MovieRepository : IMovieRepository
     {
 
-        private readonly IDbConnectionFactory _connectionFactory;
+        private readonly IDbConnectionFactory _dbConnectionFactory;
 
         public MovieRepository(IDbConnectionFactory connectionFactory)
         {
-            _connectionFactory = connectionFactory;
+            _dbConnectionFactory = connectionFactory;
         }
 
         public async Task<bool> CreateAsync(Movie movie)
         {
-            using var connection = await _connectionFactory.CreateConnectionAsync();
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
             using var transaction = connection.BeginTransaction();
 
             var result = await connection.ExecuteAsync(new CommandDefinition(
@@ -42,24 +42,67 @@ namespace Movies.Application.Repository
             return result > 0;
         }
 
-        public Task<bool> DeleteByIdAsync(Guid id)
+        public async Task<bool> DeleteByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+            using var transaction = connection.BeginTransaction();
+
+            await connection.ExecuteAsync(new CommandDefinition(
+                """
+                delete from genres_new where moviesId = @id
+                """, new { id }
+                ));
+
+            var result = await connection.ExecuteAsync(new CommandDefinition(
+                """
+                delete from movies where id = @id
+                """, new { id }
+                ));
+
+            transaction.Commit();
+
+            return result > 0;
         }
 
-        public Task<bool> ExistsByIdAsync(Guid id)
+        public async Task<bool> ExistsByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+            return await connection.ExecuteScalarAsync<bool>(new CommandDefinition(
+                """
+                select count(1) from movies where id = @id
+                """, new { id }));
         }
 
-        public Task<IEnumerable<Movie>> GetAllAsync()
+        public async Task<IEnumerable<Movie>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+            var result = await connection.QueryAsync(new CommandDefinition(
+                """
+                select
+                    m.*,
+                    string_agg(g.name, ',') as genres
+                from movies as m
+                inner join genres_new as g
+                    on m.id = g.movieId
+                GROUP BY m.id, m.slug, m.title, m.yearofrelease
+                """
+                ));
+
+
+            // Map the result to the Movie objects
+            return result.Select(x => new Movie
+            {
+                // Assign values while ensuring proper handling of nulls
+                Id = x.id ?? Guid.NewGuid(),  // Use a default Guid if null
+                Title = x.title ?? string.Empty,  // Use empty string if title is null
+                YearOfRelease = x.yearofrelease ?? 0,  // Use default value 0 if null
+                Genres = string.IsNullOrEmpty(x.genres) ? new List<string>() : Enumerable.ToList(x.genres.Split(','))  // Split genres string into a list
+            });
         }
 
         public async Task<Movie?> GetByIdAsync(Guid id)
         {
-            using var connection = await _connectionFactory.CreateConnectionAsync();
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
             var movie = await connection.QuerySingleOrDefaultAsync<Movie>(new CommandDefinition(
                 """
                 select * from movies where id = @id
@@ -85,7 +128,7 @@ namespace Movies.Application.Repository
         public async Task<Movie?> GetBySlugAsync(string slug)
         {
             // open connection
-            using var connection = await _connectionFactory.CreateConnectionAsync();
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
             var movie = await connection.QueryFirstOrDefaultAsync<Movie>(new CommandDefinition(
                 """
                 select * from movies where slug = @slug
@@ -109,9 +152,36 @@ namespace Movies.Application.Repository
             return movie;
         }
 
-        public Task<bool> UpdateAsync(Movie movie)
+        public async Task<bool> UpdateAsync(Movie movie)
         {
-            throw new NotImplementedException();
+            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+            using var transaction = connection.BeginTransaction();
+
+            await connection.ExecuteAsync(new CommandDefinition(
+                """
+                delete from genres_new where movieId = @id
+                """, new { id = movie.Id }
+                ));
+
+            foreach (var genre in movie.Genres)
+            {
+                await connection.ExecuteAsync(new CommandDefinition(
+                    """
+                    insert into genres_new (movieId, name)
+                    values(@MovieId, @Name)
+                    """, new { movieId = movie.Id, Name = genre }
+                    ));
+            }
+
+            var result = await connection.ExecuteAsync(new CommandDefinition(
+                """
+                update movies set slug = @Slug, title = @title, yearofrelease = @yearOfRelease
+                where id = @Id
+                """, movie
+                ));
+            transaction.Commit();
+            return result > 0;
+
         }
 
 
